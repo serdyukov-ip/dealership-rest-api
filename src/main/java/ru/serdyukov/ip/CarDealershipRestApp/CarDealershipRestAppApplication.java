22fa19d013afb3f46f76e5c7526ee7334b7469aa
serdyukov-ip <serdyukov-ip@yandex.ru>
2024-03-27 20:46:37 +0300

    initial commit

---
+++ src/main/java/ru/serdyukov/ip/CarDealershipRestApp/CarDealershipRestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.serdyukov.ip.CarDealershipRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/serdyukov/ip/CarDealershipRestApp/CarDealershipRestAppApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.serdyukov.ip.CarDealershipRestApp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(true, true);
+	}
+
+}
