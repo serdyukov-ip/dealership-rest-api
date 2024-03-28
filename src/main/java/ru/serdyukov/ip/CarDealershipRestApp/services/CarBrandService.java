a0fe15a6ae8de4c7b50470f3537bb5bd4b14bf78
serdyukov-ip <serdyukov-ip@yandex.ru>
2024-03-28 22:43:41 +0300

    service classes created

---
+++ src/main/java/ru/serdyukov/ip/CarDealershipRestApp/services/CarBrandService.java
@@ -0,0 +1,37 @@
+package ru.serdyukov.ip.CarDealershipRestApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.serdyukov.ip.CarDealershipRestApp.models.CarBrand;
+import ru.serdyukov.ip.CarDealershipRestApp.models.Manager;
+import ru.serdyukov.ip.CarDealershipRestApp.repositories.CarBrandRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class CarBrandService {
+
+    private final CarBrandRepository carBrandRepository;
+
+    @Autowired
+    public CarBrandService(CarBrandRepository carBrandRepository) {
+        this.carBrandRepository = carBrandRepository;
+    }
+
+    public List<CarBrand> findAll() {
+        return carBrandRepository.findAll();
+    }
+
+    public CarBrand findOne(int id) {
+        Optional<CarBrand> foundCarBrand = carBrandRepository.findById(id);
+        return foundCarBrand.orElse(null);
+    }
+
+    @Transactional
+    public void save(CarBrand carBrand) {
+        carBrandRepository.save(carBrand);
+    }
+}
---
+++ src/main/java/ru/serdyukov/ip/CarDealershipRestApp/services/CarService.java
@@ -0,0 +1,37 @@
+package ru.serdyukov.ip.CarDealershipRestApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.serdyukov.ip.CarDealershipRestApp.models.Car;
+import ru.serdyukov.ip.CarDealershipRestApp.models.Manager;
+import ru.serdyukov.ip.CarDealershipRestApp.repositories.CarRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> findAll() {
+        return carRepository.findAll();
+    }
+
+    public Car findOne(int id) {
+        Optional<Car> foundCar = carRepository.findById(id);
+        return foundCar.orElse(null);
+    }
+
+    @Transactional
+    public void save(Car car) {
+        carRepository.save(car);
+    }
+}
