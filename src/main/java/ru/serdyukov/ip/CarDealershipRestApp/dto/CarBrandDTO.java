139e6ecc94d08555e63bb24ab67bf37dab4df7cf
serdyukov-ip <serdyukov-ip@yandex.ru>
2024-04-07 22:32:28 +0300

    fix bug for CarBrand model

--- src/main/java/ru/serdyukov/ip/CarDealershipRestApp/dto/CarBrandDTO.java
+++ src/main/java/ru/serdyukov/ip/CarDealershipRestApp/dto/CarBrandDTO.java
@@ -11,7 +11,6 @@ public class CarBrandDTO {
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
 }
